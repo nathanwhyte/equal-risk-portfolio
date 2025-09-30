55c42b1b2106b9d63284a1fbd4cb2a1698d9b21e
Nathan Whyte <nathanwhyte35@gmail.com>
2025-09-30 10:28:04 -0500

    Disable input autocomplete, placeholder result styling

--- app/controllers/search/tickers_controller.rb
+++ app/controllers/search/tickers_controller.rb
@@ -1,10 +1,12 @@
 class Search::TickersController < ApplicationController
   def search
+    # TODO: update to call Polygon
+    #       pass back logo url, ticker, and name
     tickers = %w[AAPL MSFT GOOGL AMZN TSLA]
     if params[:query].present?
       @tickers = tickers.select { |ticker| ticker.include?(params[:query].upcase) }
     else
-      @tickers = tickers
+      @tickers = []
     end
   end
 end
