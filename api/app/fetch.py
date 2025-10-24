bfe4265a0daa7a6f52701fcb1df0772b38ceb347
Nathan Whyte <nathanwhyte35@gmail.com>
2025-10-24 22:36:49 +0000

    Move Polygon API key to environment variable

--- api/app/fetch.py
+++ api/app/fetch.py
@@ -1,13 +1,11 @@
+import os
 from datetime import date, datetime, timedelta, timezone
 from uuid import uuid4
 
-from polygon import (
-    RESTClient,  # pyright: ignore[reportAttributeAccessIssue, reportUnknownVariableType]
-)
 from sqlalchemy import text
 from sqlalchemy.engine import Engine
 
-polygon_client = RESTClient("pfVuWDAv5Y4Vs1JkIWlEMtGyfFhRdrw9")
+polygon_client = os.getenv("POLYGON_API_KEY")
 
 today = date.today()
 yesterday = today - timedelta(days=1)
