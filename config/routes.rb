39dbe83bdda59fbfcce2a86879dcac86d364035b
Nathan Whyte <nathanwhyte35@gmail.com>
2025-10-25 23:38:42 -0500

    Add sign in/out button in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,6 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  resource :session
+  resources :passwords, param: :token
 
   resources :portfolios
 
