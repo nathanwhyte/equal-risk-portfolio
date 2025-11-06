e77bf936dfb31292bcfcf79ea0fe6845363b0240
Nathan Whyte <nathanwhyte35@gmail.com>
2025-11-06 13:27:07 -0600

    Remove timestamp metadata cols for close_prices

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_04_154309) do
 
   create_table "close_prices", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.float "close"
-    t.datetime "created_at", null: false
     t.string "date"
     t.string "ticker"
-    t.datetime "updated_at", null: false
     t.index ["ticker"], name: "index_close_prices_on_ticker"
   end
 
